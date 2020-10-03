272abf341ce90ba9aca6946ed04c2c630caeadc3
Thomas <thombsaway@gmail.com>
2020-10-03 15:57:42 +1000

    Make insertion just a function in the sort module, rather than a class.

--- algorithms/sort/insertion.py
+++ algorithms/sort/insertion.py
@@ -1,44 +1,39 @@
 
-class Insertion:
+def insertion(a: list, order='ascending') -> list:
+    '''Sorts the list a in ascending order by default
 
-    def __init__(self):
-        pass
+    Parameters
+    ----------
+    a: a list of integers
+    order: a string, can be ascending or descending
 
-    def sort(self, a: list, order='ascending') -> list:
-        '''Sorts the list a in ascending order by default
+    Returns
+    -------
+    A list of sorted integers
+    '''
 
-        Parameters
-        ----------
-        a: a list of integers
-        order: a string, can be ascending or descending
+    if order == 'ascending':
+        for j in range(1, len(a)): 
+            key = a[j]
+            i = j - 1
 
-        Returns
-        -------
-        A list of sorted integers
-        '''
+            # shuffle everything to the right, while a[i] is > key
+            while i >= 0 and a[i] > key:
+                a[i + 1] = a[i]
+                i = i - 1
+            
+            a[i + 1] = key
 
-        if order == 'ascending':
-            for j in range(1, len(a)): 
-                key = a[j]
-                i = j - 1
+    else:
+        for j in range(1, len(a)): 
+            key = a[j]
+            i = j - 1
 
-                # shuffle everything to the right, while a[i] is > key
-                while i >= 0 and a[i] > key:
-                    a[i + 1] = a[i]
-                    i = i - 1
-                
-                a[i + 1] = key
+            # shuffle everything to the right, while a[i] is < key
+            while i >= 0 and a[i] < key:
+                a[i + 1] = a[i]
+                i = i - 1
+            
+            a[i + 1] = key
 
-        else:
-            for j in range(1, len(a)): 
-                key = a[j]
-                i = j - 1
-
-                # shuffle everything to the right, while a[i] is < key
-                while i >= 0 and a[i] < key:
-                    a[i + 1] = a[i]
-                    i = i - 1
-                
-                a[i + 1] = key
-
-        return a
+    return a
