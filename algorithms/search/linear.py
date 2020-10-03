1d12f2100cb061048d5f6169f2154c5b73468e41
Thomas <thombsaway@gmail.com>
2020-10-03 15:50:57 +1000

    Add doc for linear.find

--- algorithms/search/linear.py
+++ algorithms/search/linear.py
@@ -5,7 +5,19 @@ class LinearSearch:
         pass
 
     def find(self, a: list, v: int) -> int:
+        '''Returns the index of the value v, in list a 
 
+        If v is not found in a, then -1 is returned.
+
+        Parameters
+        ----------
+        a: a list of integers
+        v: an integer
+
+        Returns
+        -------
+        The index of value v in list a
+        '''
         for j in range(len(a)):
             if a[j] == v:
                 return j
