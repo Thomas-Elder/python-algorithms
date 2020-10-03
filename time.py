6870f0596f013d235b9222916e190ac18ac27e57
Thomas <thombsaway@gmail.com>
2020-10-03 15:57:26 +1000

    Remove function for executionTime, adding nothing

--- time.py
+++ time.py
@@ -2,20 +2,16 @@
 from timeit import default_timer as timer
 from random import sample
 
-from algorithms.sort import insertion
+from algorithms.sort.insertion import insertion 
 
 # logging
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def executionTime(func , a: list):
-
-    start = timer()
-    func(a)
-    end = timer()
-
-    logging.info(f'Exection time: {round(end-start, 2)} seconds')
-
 a = sample(range(1, 100000), 10000)
-sorter = insertion.Insertion()
-executionTime(sorter.sort, a)
+
+start = timer()
+insertion(a, order='ascending')
+end = timer()
+
+logging.info(f'Exection time: {round(end-start, 2)} seconds')
