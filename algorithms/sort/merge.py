9ce06716199310e0c2c7510b07ea044cf7d4123f
Thomas <thombsaway@gmail.com>
2020-10-04 21:09:58 +1100

    Implement merge sort

--- algorithms/sort/merge.py
+++ algorithms/sort/merge.py
@@ -12,4 +12,33 @@ def merge(a: list) -> list:
     A list of sorted integers
     '''
 
+    if len(a) > 1:
+        mid = len(a) // 2
+        L = a[:mid] 
+        R = a[mid:]
+  
+        merge(L) 
+        merge(R)
+  
+        i = j = k = 0
+          
+        while i < len(L) and j < len(R): 
+            if L[i] < R[j]: 
+                a[k] = L[i] 
+                i += 1
+            else: 
+                a[k] = R[j]
+                j += 1
+            k += 1
+          
+        while i < len(L): 
+            a[k] = L[i] 
+            i += 1
+            k += 1
+          
+        while j < len(R): 
+            a[k] = R[j] 
+            j += 1
+            k += 1
+
     return a
