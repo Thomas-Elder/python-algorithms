fd4568fd6c4c1251be97643db921481e3b4eaf37
Thomas Elder <thombsaway@gmail.com>
2020-10-06 15:20:08 +1100

    Printing lists before and after sorting

--- time.py
+++ time.py
@@ -1,46 +1,73 @@
 
 from timeit import default_timer as timer
 from random import sample
+import random
 
 from algorithms.sort.insertion import insertion
 from algorithms.sort.merge import merge
 from algorithms.sort.bubble import bubble
 from algorithms.search.linear import linearSearch
 
+
 # logging
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-x, y = 100000, 10000
+def unsortedList():
+    a = []
+    for i in range(1, 1000):
+        a.append(random.randint(1, 20))
+
+    return a
 
 # Insertion sort
-a = sample(range(1, x), y)
+a = unsortedList()
+print(f'Unsorted list:')
+print(a)
+
 start = timer()
 insertion(a, order='ascending')
 end = timer()
 
 print(f'Insertion sort exection time: {round(end-start, 2)} seconds')
+print(f'Sorted list:')
+print(a)
 
 # Merge sort
-a = sample(range(1, x), y)
+a = unsortedList()
+print(f'Unsorted list:')
+print(a)
+
 start = timer()
 merge(a)
 end = timer()
 
 print(f'Merge sort exection time: {round(end-start, 2)} seconds')
+print(f'Sorted list:')
+print(a)
 
-# Merge sort
-a = sample(range(1, x), y)
+# Bubble sort
+a = unsortedList()
+print(f'Unsorted list:')
+print(a)
+
 start = timer()
 bubble(a)
 end = timer()
 
 print(f'Bubble sort exection time: {round(end-start, 2)} seconds')
+print(f'Sorted list:')
+print(a)
 
 # Linear search
-a = sample(range(1, x), y)
+a = unsortedList()
+print(f'Unsorted list:')
+print(a)
+
 start = timer()
 linearSearch(a, 100)
 end = timer()
 
 print(f'Linear search exection time: {round(end-start, 2)} seconds')
+print(f'Sorted list:')
+print(a)
