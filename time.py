5b78072ad554f863066b8c586037d8cd656f720b
Thomas Elder <thombsaway@gmail.com>
2020-10-06 15:09:11 +1100

    Update time with bubble sort timing, and variabalise size of the array

--- time.py
+++ time.py
@@ -4,14 +4,17 @@ from random import sample
 
 from algorithms.sort.insertion import insertion
 from algorithms.sort.merge import merge
+from algorithms.sort.bubble import bubble
 from algorithms.search.linear import linearSearch
 
 # logging
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+x, y = 100000, 10000
+
 # Insertion sort
-a = sample(range(1, 10000000), 1000000)
+a = sample(range(1, x), y)
 start = timer()
 insertion(a, order='ascending')
 end = timer()
@@ -19,15 +22,23 @@ end = timer()
 print(f'Insertion sort exection time: {round(end-start, 2)} seconds')
 
 # Merge sort
-a = sample(range(1, 10000000), 1000000)
+a = sample(range(1, x), y)
 start = timer()
 merge(a)
 end = timer()
 
 print(f'Merge sort exection time: {round(end-start, 2)} seconds')
 
+# Merge sort
+a = sample(range(1, x), y)
+start = timer()
+bubble(a)
+end = timer()
+
+print(f'Bubble sort exection time: {round(end-start, 2)} seconds')
+
 # Linear search
-a = sample(range(1, 10000000), 1000000)
+a = sample(range(1, x), y)
 start = timer()
 linearSearch(a, 100)
 end = timer()
