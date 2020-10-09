1aa628f51b0aee1df8aa72a37ce7fea8580218e6
Thomas <thombsaway@gmail.com>
2020-10-09 15:11:47 +1100

    Add recursive bubble sort to time

--- time.py
+++ time.py
@@ -6,12 +6,14 @@ import random
 from algorithms.sort.insertion import insertion
 from algorithms.sort.merge import merge
 from algorithms.sort.bubble import bubble
+from algorithms.sort.bubble import bubble_recursive
 from algorithms.search.linear import linearSearch
 
 
 # logging
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.disable()
 
 def unsortedList():
     a = []
@@ -23,52 +25,40 @@ def unsortedList():
 
 # Insertion sort
 a = unsortedList()
-print(f'Unsorted list:')
-print(a)
-
 start = timer()
 insertion(a, order='ascending')
 end = timer()
 
 print(f'Insertion sort exection time: {round(end-start, 2)} seconds')
-print(f'Sorted list:')
-print(a)
 
 # Merge sort
 a = unsortedList()
-print(f'Unsorted list:')
-print(a)
-
 start = timer()
 merge(a)
 end = timer()
 
 print(f'Merge sort exection time: {round(end-start, 2)} seconds')
-print(f'Sorted list:')
-print(a)
 
 # Bubble sort
 a = unsortedList()
-print(f'Unsorted list:')
-print(a)
-
 start = timer()
 bubble(a)
 end = timer()
 
 print(f'Bubble sort exection time: {round(end-start, 2)} seconds')
-print(f'Sorted list:')
-print(a)
+
+# Bubble sort - recursive
+a = unsortedList()
+start = timer()
+bubble_recursive(a)
+end = timer()
+
+print(f'Bubble sort exection time: {round(end-start, 2)} seconds')
 
 # Linear search
 a = unsortedList()
-print(f'Unsorted list:')
-print(a)
-
 start = timer()
 linearSearch(a, 100)
 end = timer()
 
 print(f'Linear search exection time: {round(end-start, 2)} seconds')
-print(f'Sorted list:')
-print(a)
